bc4ff84336575ca92fdc2b0a0348aef0bed8065c
Konstantin <94466218+RENOMIZER@users.noreply.github.com>
2026-01-30 22:34:43 +0300

    Add about and proxy (partialy) sections

--- src/screens/scripts/settings.js
+++ src/screens/scripts/settings.js
@@ -1,7 +1,33 @@
 const $ = (id) => document.getElementById(id)
+let language = window.electronAPI.language
 
-$('langOpt').addEventListener('click', () => { if ($('langOpt').checked) $('workAreaHeader').innerHTML = 'Language' })
-$('themeOpt').addEventListener('click', () => { if ($('themeOpt').checked) $('workAreaHeader').innerHTML = 'Themes' })
-$('proxyOpt').addEventListener('click', () => { if ($('proxyOpt').checked) $('workAreaHeader').innerHTML = 'Proxy' })
-$('depsOpt').addEventListener('click', () => { if ($('depsOpt').checked) $('workAreaHeader').innerHTML = 'Dependencies' })
-$('aboutOpt').addEventListener('click', () => { if ($('aboutOpt').checked) $('workAreaHeader').innerHTML = 'About' })
+$('hostTxt').textContent = language.host
+$('portTxt').textContent = language.port
+$('protoTxt').textContent = language.proto
+// $('enableTxt').textContent = language.enable
+
+$('menu').addEventListener('click', () => { 
+    if ($('langOpt').checked) {
+        $('workAreaHeader').innerHTML = 'Language'
+    }
+
+    if ($('themeOpt').checked) $('workAreaHeader').innerHTML = 'Themes'
+
+    if ($('proxyOpt').checked) {
+        $('workAreaHeader').innerHTML = 'Proxy'
+        $('proxySection').hidden = false
+    }
+    else {
+        $('proxySection').hidden = true
+    }
+
+    if ($('depsOpt').checked) $('workAreaHeader').innerHTML = 'Dependencies'
+
+    if ($('aboutOpt').checked) {
+        $('workAreaHeader').innerHTML = 'About'
+        $('aboutSection').hidden = false
+    }
+    else {
+        $('aboutSection').hidden = true
+    }
+})
